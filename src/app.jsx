4e63d5065ac08e27b98839bc9a1a5dc5b184bcfb
MING0604 <1206710179@qq.com>
2020-09-23 20:10:58 +0800

    webpack脚手架的搭建

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import 'font-awesome/css/font-awesome.min.css'
+import './index.css'
+import './index.scss'
+ReactDOM.render(
+    <div>
+        <i className='fa fa-user-circle-o'></i>
+        <h1>Hello jsx</h1>
+    </div>
+    ,
+    document.querySelector('#app')
+)
---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/app.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath:'/dist/',
+    filename: 'js/app.js'
+  },
+  plugins: [
+    // 处理HTML文件
+    new HtmlWebpackPlugin({
+      template:'./src/index.html'
+    }),
+    // 提出css文件
+    new ExtractTextPlugin("css/[name].css"),
+    // 提出公共模块
+    new webpack.optimize.CommonsChunkPlugin({
+      name:'common',
+      filename:'js/base.js'
+    })
+  ],
+  module: {
+    rules: [
+      // react语法处理
+      {
+        test: /\.m?jsx$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env','react']
+          }
+        }
+      },
+      // css语法处理
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      },
+      // sass文件的处理
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      // 图片配置
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name:'resource/[name].[ext]'
+            },
+          },
+        ],
+      },
+      // 字体图标配置
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name:'resource/[name].[ext]'
+            },
+          },
+        ],
+      },
+      
+    ]
+  },
+  devServer: {
+    port:8086
+  }
+};
