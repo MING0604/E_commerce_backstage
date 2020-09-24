35bf2e3daec3b111e789b8db0343f46bff5a99d5
MING0604 <1206710179@qq.com>
2020-09-24 13:42:50 +0800

    Switch的使用

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,63 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
-import 'font-awesome/css/font-awesome.min.css'
-import './index.css'
-import './index.scss'
+import {HashRouter as Router,Route,Link,Switch} from 'react-router-dom'
+class A extends Component {
+    constructor(props){
+        super(props)
+        
+    }
+   render() {
+       console.log(this.props.match.params)
+        return (
+            <div>
+                Component A
+                <Switch>
+                     <Route exact path={`${this.props.match.path}`} render={(route)=>{
+                        return <div>当前组件无参数</div>
+                    }}></Route>
+                    <Route path={`${this.props.match.path}/:id`} render={(route)=>{
+                        return <div>当前组件的参数是 {route.match.params.id}</div>
+                    }}></Route>
+                    
+                </Switch>
+            </div>
+        )
+    }
+}
+class B extends Component {
+    render() {
+        return (
+            <div>Component B</div>
+        )
+    }
+}
+class Wrapper extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                <Link to='/a/123'>有参数组件A</Link>
+                <br/>
+                <Link to='/a'>无参数组件A</Link>
+                <br/>
+                <Link to='/b'>组件B</Link>
+                {this.props.children}
+            </div>
+        )
+    }
+}
+
 ReactDOM.render(
-    <div>
-        <i className='fa fa-user-circle-o'></i>
-        <h1>Hello jsx</h1>
-    </div>
-    ,
+    <Router>
+        <Wrapper>
+            <Route path="/a" component={A}></Route>
+            <Route path='/b' component={B}></Route>
+        </Wrapper>       
+    </Router>
+,
     document.querySelector('#app')
 )
